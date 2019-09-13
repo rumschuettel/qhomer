0d97d9021327c30cf5be3aa103dc64ea63007da1
Gal <galvien@gmail.com>
2019-09-14 00:12:49 +0300

    delete old local main file

--- main.py
+++
@@ -1,8 +0,0 @@
-from plot_tools import *
-from VectorQraphics import *
-
-file = "Resources/vector_line.svg"
-
-lines = get_points(file) 
-for line in lines:
-    plot('',map(lambda x: real(x)),map(lambda y:imag(y)  ))
