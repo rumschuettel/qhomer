257cd223f8fd1a5bbb691f0fddae7c6ecaf4aeed
Gal <galvien@gmail.com>
2019-09-14 01:09:14 +0300

    add jsonify func to vector graphics library
    this works git push go to sleep

--- VectorQraphics.py
+++ VectorQraphics.py
@@ -1,10 +1,12 @@
 import svgpathtools
 import math
 from numpy import real, imag
-
+import json
 
 # input: list of svgpathtools.path objects, each containing a list of segments
 # output: list of lists of points in 2D space
+
+
 def get_points(file, density):
     #TODO: calculate density automatically for target point #
     paths = svgpathtools.svg2paths2(file)
@@ -16,27 +18,47 @@ def get_points(file, density):
             frac = 1.0/count
             for i in range(1, count):
                 pathpoints.append(seg.point(frac*i))
-        res.append(pathpoints)
-    f = lambda x: x.start
-    starts = list(map(lambda i: f(i),paths[0]))
+        if len(pathpoints) > 0:
+            res.append(pathpoints)
+
+    def f(x): return x.start
+    starts = list(map(lambda i: f(i), paths[0]))
     return res, starts
 
 
 def flip_x(input):
     """flips the real values of all of the points"""
+    # DOES NOT WORK DO NOT TOUCH STAY AWAY FROM THIS ONE CAUSE IT BITES
     res = []
     for line in input:
-        max_x = real(max(line , key=(lambda x: (real(x)))))
-        res.append(list(map(lambda x: complex(max_x-real(x),imag(x)),line)))
+        max_x = real(max(line, key=(lambda x: (real(x)))))
+        res.append(list(map(lambda x: complex(max_x-real(x), imag(x)), line)))
     return res
+
 
 def flip_y(input):
     """flips the imaginary values of all of the points, do THAT for good results"""
     res = []
     for line in input:
         #max_y = imag(max(line , key=(lambda y: (imag(y)))))
-        res.append(list(map(lambda y: complex(real(y),-imag(y)),line)))
+        res.append(list(map(lambda y: complex(real(y), -imag(y)), line)))
     return res
+
+
 def offset(line, dx, dy):
-    return list(map(lambda p:complex(real(p)+dx,imag(p)+dy),line))
-        
+    return list(map(lambda p: complex(real(p)+dx, imag(p)+dy), line))
+
+
+def jsonify(lines):
+    res = []
+    for line in lines:
+        tmp = []
+        for point in line:
+            pt = []
+            pt.append(str(real(point)))
+            pt.append(str(imag(point)))
+            tmp.append(pt)
+        if len(tmp) > 0:
+            res.append(tmp)
+
+    return json.dumps(res)
