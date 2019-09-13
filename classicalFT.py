865fd848e986afb49edbd61df39b8afb347f4fce
Amanda Matthes <amanda.matthes@gmail.com>
2019-09-13 22:56:05 +0200

    added classicalFT_demo

--- classicalFT.py
+++ classicalFT.py
@@ -1,8 +1,6 @@
 
-from pngTools import *
 import math
 import cmath
-from pprint import pprint
 
 def ft(x):
     N = len(x)
@@ -28,32 +26,3 @@ def ift(y):
     return x
 
 
-"""
-Get gray vector
-"""
-
-filename        = 'Resources/test.png'
-img             = read(filename)
-grayVector_in   = rgbImage2grayVector(img)
-
-
-"""
-Fourier transform
-"""
-grayVector_ft       = ft(grayVector_in)
-
-
-"""
-Inverse Fourier transform
-"""
-grayVector_ft_ift   = ift(grayVector_ft)
-grayVector_out      = [abs(element) for element in grayVector_ft_ift]
-
-"""
-Show difference in floating point values and images
-"""
-pprint(grayVector_in)
-pprint(grayVector_out)
-
-show(grayVector2rgbImage(grayVector_in))
-show(grayVector2rgbImage(grayVector_out))
---
+++ classicalFT_demo.py
@@ -0,0 +1,33 @@
+from classicalFT import *
+from pngTools import *
+from pprint import pprint
+
+"""
+Get gray vector
+"""
+
+filename        = 'Resources/test.png'
+img             = read(filename)
+grayVector_in   = rgbImage2grayVector(img)
+
+
+"""
+Fourier transform
+"""
+grayVector_ft       = ft(grayVector_in)
+
+
+"""
+Inverse Fourier transform
+"""
+grayVector_ft_ift   = ift(grayVector_ft)
+grayVector_out      = [abs(element) for element in grayVector_ft_ift]
+
+"""
+Show difference in floating point values and images
+"""
+pprint(grayVector_in)
+pprint(grayVector_out)
+
+show(grayVector2rgbImage(grayVector_in))
+show(grayVector2rgbImage(grayVector_out))
