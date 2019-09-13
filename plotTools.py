ed6edae6e608969c5aaa5c1c0ee3dd4de55cbd92
Amanda Matthes <amanda.matthes@gmail.com>
2019-09-13 22:54:35 +0200

    Update plotTools.py

--- plotTools.py
+++ plotTools.py
@@ -16,13 +16,13 @@ PINK    = '#f141f4'
 GREY    = '#5b5f66'
 BLACK   = '#000000'
 
-font = {'fontname':'Helvetica',
+font = {
 'weight': 'normal',
 'size': 13,
 }
 
 def plot(title, x, y, x_err=None, y_err=None,
-        colour = ORANGE, marker = '.', markersize = 4, linestyle = '',
+        colour = DBLUE, marker = '.', markersize = 4, linestyle = '',
         xlabel = '', ylabel = '',
         show = True):
 
@@ -41,4 +41,4 @@ def plot(title, x, y, x_err=None, y_err=None,
 # x = np.array([0, 1, 2, 3, 4, 5])
 # y = np.array([i**2 + random.random() for i in x])
 
-# plot('test', x, y)
+# plot('Test', x, y)
